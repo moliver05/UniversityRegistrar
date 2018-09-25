737f0582f6b63a8d3aa156147b3379264723c502
Mark <mo.mangahas@yahoo.com>
2018-09-25 09:56:13 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Solution.Models;
+namespace UniversityRegistrar.Solution.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
